4647da6e623659e8dbc5fa2779dd48eeb86826f5
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-06-14 13:48:01 -0700

    Add JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './style.css';
+const form = document.getElementById('list-form');
+const lists = document.getElemenybyId('list-container');
+const inputfield = document.getElementById('input-id');
+const refresh = document.getElementById('refresh');
+const enter = document.getElementById('enter');
+const remove = document.getElementById('clear');
+const tasks = [];
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+   var id = i;
+   i++;
+   const todo = new List(input.value,Boolean,id);
+   this.tasks = [...tasks, List];
+   console.log(todo);
+});
+//Object Instances;
+class List {
+    constructor(description,complete,id) {
+    this.description = description,
+    this.complete = Boolean,
+    this.id = id
+    }
+  }
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: 'index.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
