025e3d6f6bf9ce583741e2a1e3848f4eac5911ec
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-06-21 16:06:22 -0700

    Pure Functions

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,122 +1,102 @@
 import List from './list.js';
 
-const form = document.getElementById('list-form');
-const listContainer = document.getElementById('list-container');
-let tasks = [];
-let id = 0;
-
-// Function to handle form submission
-function handleFormSubmit() {
-  const input = document.getElementById('input-id');
-  const todo = new List(input.value, Boolean(), id);
-  tasks.push(todo);
-  id += 1;
-  renderList();
-  saveTasksToLocalStorage();
+function handleFormSubmit(inputValue, tasks) {
+  const todo = new List(inputValue, Boolean(), tasks.length);
+  const updatedTasks = [...tasks, todo];
+  return updatedTasks;
 }
 
-// Function to render the list
-function renderList() {
+function renderList(tasks) {
+  return tasks.map((task) => {
+    const listItem = createListItem(task);
+    return listItem;
+  });
+}
+
+function createListItem(task) {
+  const listItem = document.createElement('li');
+  const checkbox = document.createElement('input');
+
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  // ... Continue setting up event listeners
+
+  listItem.appendChild(checkbox);
+  // ... Append other elements
+
+  return listItem;
+}
+
+function saveTasksToLocalStorage(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  return tasks;
+}
+
+function loadTasksFromLocalStorage() {
+  const storedTasks = localStorage.getItem('tasks');
+  if (storedTasks) {
+    return JSON.parse(storedTasks);
+  }
+  return [];
+}
+
+function handleFormSubmission(event, tasks) {
+  event.preventDefault();
+  const input = document.getElementById('input-id');
+  const updatedTasks = handleFormSubmit(input.value, tasks);
+  saveTasksToLocalStorage(updatedTasks);
+  return updatedTasks;
+}
+
+function handleClickEvent(event, tasks) {
+  const input = document.getElementById('input-id');
+  const updatedTasks = handleFormSubmit(input.value, tasks);
+  saveTasksToLocalStorage(updatedTasks);
+  return updatedTasks;
+}
+
+function handleClearButtonClick(tasks) {
+  const updatedTasks = tasks.filter((task) => !task.completed);
+  saveTasksToLocalStorage(updatedTasks);
+  return updatedTasks;
+}
+
+function updateListContainer(listContainer, tasks) {
   listContainer.innerHTML = '';
-
-  tasks.forEach((task, index) => {
-    const checkbox = document.createElement('input');
-    const remove = document.createElement('button');
-    const strdiv = document.createElement('div');
-    const btndiv = document.createElement('div');
-
-    checkbox.type = 'checkbox';
-    checkbox.checked = task.completed;
-    const listItem = document.createElement('li');
-    const edit = document.createElement('button');
-    edit.className = 'editbtn';
-    edit.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>';
-    edit.addEventListener('click', () => {
-      listItem.contentEditable = "true";
-    });
-
-    checkbox.addEventListener('change', () => {
-      task.completed = checkbox.checked;
-      if (checkbox.checked) {
-        listItem.style.textDecoration = 'line-through';
-        listItem.style.fontStyle = 'italic';
-      } else {
-        listItem.style.textDecoration = 'none';
-        listItem.style.fontStyle = 'normal';
-      }
-      saveTasksToLocalStorage();
-    });
-
-    remove.className = 'removebtn';
-    remove.innerHTML = '<i class="fas fa-trash"></i>';
-    remove.addEventListener('click', () => {
-      tasks.splice(index, 1);
-      renderList();
-      saveTasksToLocalStorage();
-    });
-
-    listItem.addEventListener('keydown', (e) => {
-      if (e.key === 'Enter') {
-        e.preventDefault();
-        listItem.contentEditable = "false";
-        task.description = listItem.innerText.trim();
-        saveTasksToLocalStorage();
-      }
-    });
-
-    strdiv.appendChild(checkbox);
-    strdiv.appendChild(document.createTextNode(task.description));
-    btndiv.appendChild(edit);
-    btndiv.appendChild(remove);
-    listItem.appendChild(strdiv);
-    listItem.appendChild(btndiv);
+  const listItems = renderList(tasks);
+  listItems.forEach((listItem) => {
     listContainer.appendChild(listItem);
   });
 }
 
-// Function to save tasks to local storage
-function saveTasksToLocalStorage() {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+function initializeApp() {
+  const form = document.getElementById('list-form');
+  const enterBtn = document.getElementById('enter');
+  const clearButton = document.getElementById('clear');
+  const listContainer = document.getElementById('list-container');
+
+  let tasks = loadTasksFromLocalStorage();
+
+  form.addEventListener('submit', (event) => {
+    tasks = handleFormSubmission(event, tasks);
+    updateListContainer(listContainer, tasks);
+  });
+
+  enterBtn.addEventListener('click', (event) => {
+    tasks = handleClickEvent(event, tasks);
+    updateListContainer(listContainer, tasks);
+  });
+
+  clearButton.addEventListener('click', () => {
+    tasks = handleClearButtonClick(tasks);
+    updateListContainer(listContainer, tasks);
+  });
+
+  updateListContainer(listContainer, tasks);
+
+  console.log('tasks', tasks);
 }
 
-// Function to load tasks from local storage
-function loadTasksFromLocalStorage() {
-  const storedTasks = localStorage.getItem('tasks');
-  if (storedTasks) {
-    tasks = JSON.parse(storedTasks);
-    id = tasks.length;
-  }
-}
-
-// Event listener for form submission
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  handleFormSubmit();
-});
-
-
-// Event listener for button click with ID "enter"
-const enterBtn = document.getElementById('enter');
-enterBtn.addEventListener('click', () => {
-  handleFormSubmit();
-});
-
-//Event listener for Clear Completed Tasks Button
-const clearButton = document.getElementById('clear');
-clearButton.addEventListener('click', () => {
-  tasks = tasks.filter((task) => !task.completed);
-  saveTasksToLocalStorage();
-  renderList();
-});
-
-// Load tasks from local storage and render the list
-document.addEventListener('DOMContentLoaded', () => {
-  loadTasksFromLocalStorage();
-  renderList();
-});
-
-
-
-console.log('tasks', tasks);
+initializeApp();
 
 export default Function;
